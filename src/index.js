650c4e39bf79838f2d272931c0b77dd1827421e2
xrxoxcxox <xrxoxcxox@gmail.com>
2019-12-03 03:48:20 +0900

    Create "static" scroll

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import React from 'react';
-const MyComponent = () => <h1>Hello from My Component</h1>;
-export default MyComponent;
+
+/** @jsx jsx */
+import { jsx, css, keyframes } from '@emotion/core';
+
+import emotion from './emotion.png';
+import figma from './figma.png';
+import gatsby from './gatsby.png';
+import netlify from './netlify.png';
+
+const images = [emotion, figma, gatsby, netlify];
+const listItems = images.map(listItem => (
+  <img src={listItem} alt="" key={listItem} />
+));
+
+const InfiniteScrollImages = () => {
+  const container = css`
+    display: flex;
+    flex-wrap: nowrap;
+    width: 100%;
+    overflow: hidden;
+  `;
+  const scroll = keyframes`
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  `;
+  const visibleArea = css`
+    display: flex;
+    flex-wrap: nowrap;
+    width: 1920px;
+    animation: ${scroll} 1s linear infinite;
+  `;
+  return (
+    <div css={container} id="foobar">
+      <div css={visibleArea}>{listItems}</div>
+      <div css={visibleArea}>{listItems}</div>
+    </div>
+  );
+};
+
+export default InfiniteScrollImages;
