675387c8dc2f737cdefe3546d8aa962048eddc9f
xrxoxcxox <xrxoxcxox@gmail.com>
2019-12-03 04:59:16 +0900

    edit component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useDimensions from 'react-use-dimensions';
 
 /** @jsx jsx */
 import { jsx, css, keyframes } from '@emotion/core';
@@ -7,37 +8,51 @@ import emotion from './emotion.png';
 import figma from './figma.png';
 import gatsby from './gatsby.png';
 import netlify from './netlify.png';
+import ronin from './ronin.jpg';
 
-const images = [emotion, figma, gatsby, netlify];
+const images = [emotion, figma, gatsby, netlify, ronin];
 const listItems = images.map(listItem => (
-  <img src={listItem} alt="" key={listItem} />
+  <img src={listItem} alt="" key={listItem} style={{ height: 200 }} />
 ));
 
-const InfiniteScrollImages = () => {
-  const container = css`
-    display: flex;
-    flex-wrap: nowrap;
-    width: 100%;
-    overflow: hidden;
-  `;
+const Scroll = () => {
+  const [ref, { width }] = useDimensions();
   const scroll = keyframes`
     0% {
       transform: translateX(0);
     }
     100% {
-      transform: translateX(-100%);
+      transform: translateX(-${width}px);
     }
   `;
   const visibleArea = css`
     display: flex;
     flex-wrap: nowrap;
-    width: 1920px;
     animation: ${scroll} 1s linear infinite;
   `;
+  console.log(width);
   return (
-    <div css={container} id="foobar">
-      <div css={visibleArea}>{listItems}</div>
-      <div css={visibleArea}>{listItems}</div>
+    <div css={visibleArea} ref={ref}>
+      {listItems}
+    </div>
+  );
+};
+
+const InfiniteScrollImages = () => {
+  const container = css`
+    display: flex;
+    flex-wrap: nowrap;
+    overflow: hidden;
+  `;
+  const kari = css`
+    width: 600px;
+  `;
+  return (
+    <div css={kari}>
+      <div css={container}>
+        <Scroll />
+        <Scroll />
+      </div>
     </div>
   );
 };
